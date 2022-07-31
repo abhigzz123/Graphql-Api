03db46a81f14281f7c7e80209b5903db7ed3f11a
abhigyanzemoso <abhigyan.pandey@zemosolabs.com>
2022-07-31 19:08:03 +0530

    querying-part

--- index.ts
+++ index.ts
@@ -1,15 +1,43 @@
 import { ApolloServer, gql } from "apollo-server";
 
+import { PRODUCTS } from "./src/core-utils/constants";
+
+// Scalar types - String , Int , Float , Boolean ,ID!
 const typeDefs = gql`
+  type Product {
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    onSale: Boolean!
+  }
+
+  input ProductFilter {
+    onSale: Boolean!
+  }
+
   type Query {
-    hello: String
+    products(filter: ProductFilter): [Product!]
+    productById(id: String): Product!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => {
-      return "Hello World";
+    products: (_parent, args, _context, _info) => {
+      let filteredProducts = PRODUCTS;
+      if (args?.filter) {
+        let { onSale } = args.filter;
+        filteredProducts = filteredProducts.filter((product) => {
+          return product?.onSale === onSale;
+        });
+      }
+      return filteredProducts;
+    },
+    productById: (_parent, args, _context, _info) => {
+      return PRODUCTS.find((product) => {
+        return product?.id === args?.id;
+      });
     },
   },
 };
@@ -17,6 +45,12 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: typeDefs,
   resolvers: resolvers,
+  context: {
+    dummy: {
+      Name: "Abhigyan Pandey",
+      Age: 23,
+    },
+  },
 });
 
 server
